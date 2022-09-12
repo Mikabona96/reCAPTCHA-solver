df679476e6552a99fa227ae6dcb3757281d1ac9d
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-09-12 09:33:44 +0300

    done

---
+++ app.js
@@ -0,0 +1,45 @@
+const { Solver } = require('2captcha');
+console.log('wait...')
+const url = window.location.href
+
+const solver = new Solver(process.env.YOUR_API_KEY);
+
+async function solveCaptcha(url) {
+  const siteKey = await document.querySelector('[data-sitekey]').getAttribute('data-sitekey');
+  console.log('starting a proccess...');
+
+  try {
+    const response = await solver.recaptcha(
+      `${siteKey}`,
+      `${url}`
+      );  
+
+    console.log('trying to find elements...');
+
+    const textArea = await document.getElementById("g-recaptcha-response")
+    const buttonB = document.querySelector('button[type="submit"]')
+    const buttonI = document.querySelector('input[type="submit"]')
+
+    if (siteKey && textArea) {
+      console.log('elements found');
+    } else {
+      console.log('elements not found');
+    }
+
+    
+    textArea.style.display = ''
+    textArea.innerHTML = response.data
+    
+    if (buttonB) {
+      buttonB.click()
+    }
+    if (buttonI) {
+      buttonI.click()
+    }
+    console.log('CreCAPTCHA solved...');
+  } catch (error) {
+    console.error(error);
+  }
+}
+solveCaptcha(url)
+module.exports = solveCaptcha
